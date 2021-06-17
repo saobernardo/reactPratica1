f09c7a096e098bbf9d7a002ad1598c27f093f3d1
saobernardo <bernardo.pinheiro@hotmail.com>
2021-06-16 22:18:03 -0300

    Añedido StyledComponents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import { props } from 'bluebird';
 import React from 'react';
+import styled from 'styled-components';
 
-function Avatar(data){
-  return(
-    <>
-      <img src={data.user.image} alt={data.user.name}/>
-      <br/>
-      <span>{data.user.name}</span>
-    </>
-  )
-}
+const Site = styled.div`
+  width: 400px;
+  height: 400px;
+  background-color: #00FF00;
+`;
+
+const Botao = styled.button`
+  font-size: 19px;
+  padding: 10px 15px;
+  border: 3px solid #FF0000;
+  color: #FF0000;
+  background-color: #FFF;
+  margin: 5px;
+  border-radius: 5px;
+`;
+
+const BotaoPequeno = styled(Botao)`
+  padding: 5px 10px;
+  font-size: 16px;
+  color: ${props => props.ativo ? '#CCC' : '#000'};
+`;
 
 function App(){
-  let user = {
-    image: "https://avatars.githubusercontent.com/u/43080002?v=4",
-    name: "Lucas São Bernardo Pinheiro" 
-  }
 
-  return <>
-    <Avatar user={user}/>
-  </>
+  return (
+    <Site>
+      <Botao>Clique aqui</Botao>
+      <BotaoPequeno ativo={true}>Clique aqui</BotaoPequeno>
+    </Site>
+  );
 }
 
 export default App;
