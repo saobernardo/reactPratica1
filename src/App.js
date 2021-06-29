8f6068a5dbdbd96e13fce65e0e2db03bd615b33d
saobernardo <bernardo.pinheiro@hotmail.com>
2021-06-28 22:02:45 -0300

    Creando lista y pasando parámetros

--- src/App.js
+++ src/App.js
@@ -3,11 +3,38 @@ import styled from 'styled-components';
 import SearchBox from './components/searchBox';
 
 function App(){
+
+  const [searchText, setSearchText] = useState('');
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList([
+      {id:1, title: 'Comprar o bolo', done: false},
+      {id:2, title: 'Pegar o cachorro no petshop', done: true},
+      {id:3, title: 'Gravar aula', done:false}
+    ]);
+  }, []);
+
+  function handleSearchInput(novoTexto){
+    setSearchText(novoTexto);
+  }
+
   return (
     <>
       <h1>Lista de Tarefas</h1>
-      <SearchBox frasePadrao="Faça sua busca..."/>
+      <SearchBox frasePadrao="Faça sua busca..." onChangeText={handleSearchInput} />
+
+      <hr />
+
+      <ul>
+        {list.map((item) =>  (
+            <li key={item.id.toString()}>{item.title}</li>
+          )
+        )}
+      </ul>
     </>
+
+
   );
 }
 
--- src/components/searchBox.js
+++ src/components/searchBox.js
@@ -1,18 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 
 const InputText = styled.input`
-    border: 2px solid #000;
-    border-radius: 5px;
-    padding: 15px;
-    font-size: 17px;
-    width: 300px;
+  border: 2px solid #000;
+  border-radius: 5px;
+  padding: 15px;
+  font-size: 17px;
+  width: 300px;
 `;
 
 function SearchBox(props){
-    return(
-        <InputText type="text" placeholder={props.frasePadrao ?? "Digite alguma coisa"} />
-    );
+
+  const [texto, setTexto] = useState('');
+
+  useEffect(() => {
+    if(props.onChangeText){
+      props.onChangeText(texto);
+    }
+  }, [texto])
+
+  return(
+    <InputText type="text" value={texto} onChange={(e) => {setTexto(e.target.value)}}
+    placeholder={props.frasePadrao ?? "Digite alguma coisa"} />
+  );
 }
 
 export default SearchBox;
